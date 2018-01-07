215f3316e0bf822a0ca223d9239a06e3c71f3336
rico <rico@pearlwallet.io>
2018-01-07 15:34:57 +0800

    update contract

--- CrowdSale.sol
+++ CrowdSale.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.16;
 
-contract owned {
+contract Owned {
     address public owner;
 
-    function owned() public {
+    function Owned() public {
         owner = msg.sender;
     }
 
@@ -17,12 +17,12 @@ contract owned {
     }
 }
 
-interface token {
+interface PltToken {
     function transfer(address receiver, uint amount) public;
     function mintToken(address receiver,uint amount) public;
 }
 
-interface EosToken {
+interface ErcToken {
     
     function allowance(address src, address guy) constant public returns (uint256);
 
@@ -35,8 +35,55 @@ interface EosToken {
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) public;
 }
 
-contract APearlCrowdsale is owned {
+/**
+ * Math operations with safety checks
+ */
+library SafeMath {
+  function mul(uint a, uint b) internal pure returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
 
+  function div(uint a, uint b) internal pure returns (uint) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint a, uint b) internal pure returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint a, uint b) internal pure returns (uint) {
+    uint c = a + b;
+    assert(c >= a);
+    return c;
+  }
+
+  function max64(uint64 a, uint64 b) internal pure returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal pure returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a < b ? a : b;
+  }
+}
+
+
+contract PearlCrowdsale is Owned {
+
+    using SafeMath for uint256;
 
     // PLT token unit.
     // Using same decimal value as ETH (makes ETH-PLT conversion much easier).
@@ -44,59 +91,52 @@ contract APearlCrowdsale is owned {
     uint256 public constant TOKEN_UNIT = 10 ** 18;
 
     // Maximum tokens offered in the sale.
-    uint256 public constant MAX_TOKENS_SOLD = 5000000000 * TOKEN_UNIT;
+    uint256 public constant MAX_TOKENS_SOLD = 500000000 * TOKEN_UNIT;
 
-    uint public ethRaised;
-    uint public eosRaised;
-    
-    uint public eosPrice;
-    uint public ethPrice;
+    uint256 public ethRaised;
+    uint256 public price;
 
-    uint public alreadyIssued;
-    uint public deadline;
-    uint public startTime;
+    uint256 public alreadyIssued;
+    uint256 public deadline;
+    uint256 public startTime;
 
-    //eostoken contact
-    EosToken public eosToken;
     //Pearl token contact
-    token public tokenReward;
+    PltToken public pltToken;
 
-    bool fundingGoalReached = false;
-    bool crowdsaleClosed = false;
+    bool crowdsaleOpening = true;
 
     event GoalReached(address recipient, uint totalAmountRaised);
     event FundTransfer(address backer, uint amount, bool isContribution);
     event RaisedEth(address sender,uint amount);
-    event RaisedEos(address sender,uint amount);
     /**
      * Constrctor function
      *
      * Setup the owner
      */
-    function APearlCrowdsale(
+    function PearlCrowdsale(
         uint _startTime,
-        address saledTokenContractAddress,
-        address eosContractAddress
+        uint _price,
+        address pltTokenContractAddress
     ) public 
     {
         startTime = _startTime;
-        deadline = _startTime + 1 * 1 days;
-        ethPrice = 0.0002 * 1 ether;
-        eosPrice = 0.003 * 1 ether;
-        tokenReward = token(saledTokenContractAddress);
-        eosToken = EosToken(eosContractAddress);
+        deadline = _startTime + (1 * 1 days);
+        price = _price;
+        pltToken = PltToken(pltTokenContractAddress);
     }
 
-
-    function setPrices(uint256 _ethPrice, uint256 _eosPrice) onlyOwner public {
-        require(_eosPrice > 0 && _ethPrice > 0);
-        ethPrice = _ethPrice * 1 ether;
-        eosPrice = _eosPrice * 1 ether;
+    function setStartTime(uint _startTime,uint _deadline) onlyOwner public {
+        startTime = _startTime;
+        deadline = _deadline;
     }
-    
-    modifier afterDeadline() { 
-        if (now >= deadline) 
-        _; 
+
+    function setPrices(uint256 _price) onlyOwner public {
+        require(_price > 0);
+        price = _price;
+    }
+
+    function closeSale() onlyOwner public {
+        crowdsaleOpening = false;
     }
 
     /**
@@ -105,34 +145,26 @@ contract APearlCrowdsale is owned {
      * The function without name is the default function that is called whenever anyone sends funds to a contract
      */
     function () payable public {
-        require(block.timestamp > startTime);
-        require(block.timestamp < deadline);
+        require(now > startTime);
+        require(now < deadline);
         require(alreadyIssued < MAX_TOKENS_SOLD);
-
-        uint amount = msg.value;
-        ethRaised += amount;
-        uint rewardAmount = (amount / ethPrice) * TOKEN_UNIT;
-        alreadyIssued += rewardAmount;
-        tokenReward.mintToken(msg.sender, rewardAmount);
+        require(crowdsaleOpening);
+        uint256 amount = msg.value;
+        uint256 rewardAmount = (amount.mul(price));
+        pltToken.mintToken(msg.sender, rewardAmount);
+        ethRaised = ethRaised.add(amount);
+        alreadyIssued = alreadyIssued.add(rewardAmount);
         RaisedEth(msg.sender,amount);
         FundTransfer(msg.sender, amount, true);
     }
 
-    function withdrawAsset() onlyOwner public {
+    function withdraw() onlyOwner public {
         owner.transfer(this.balance);
-        eosToken.transferFrom(this,owner,eosToken.balanceOf(this));
     }
 
-    /**
-     * Check if goal was reached
-     *
-     * Checks if the goal or time limit has been reached and ends the campaign
-     */
-    function checkGoalReached() afterDeadline public {
-        if (alreadyIssued >= MAX_TOKENS_SOLD) {
-            fundingGoalReached = true;
-        }
-        crowdsaleClosed = true;
+    function withdrawToken(address tokenContractAddr) onlyOwner public {
+        ErcToken token = ErcToken(tokenContractAddr);
+        token.transferFrom(this,msg.sender,token.balanceOf(this));
     }
 
 }
--- PearlToken.sol
+++ PearlToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.16;
 
-contract owned {
+contract Owned {
     address public owner;
 
-    function owned() public {
+    function Owned() public {
         owner = msg.sender;
     }
 
@@ -27,6 +27,7 @@ contract TokenERC20 {
     string public symbol;
     uint8 public decimals = 18;
     // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public constant TOKEN_UNIT = 10 ** 18;
 
     uint256 internal _totalSupply;
     mapping (address => uint256) internal _balanceOf;
@@ -50,7 +51,7 @@ contract TokenERC20 {
         string tokenSymbol
     ) public 
     {
-        _totalSupply = initialSupply * 1 ether;  // Update total supply with the decimal amount
+        _totalSupply = initialSupply * TOKEN_UNIT;  // Update total supply with the decimal amount
         _balanceOf[msg.sender] = _totalSupply;                // Give the creator all initial tokens
         name = tokenName;                                   // Set the name for display purposes
         symbol = tokenSymbol;                               // Set the symbol for display purposes
@@ -187,7 +188,54 @@ contract TokenERC20 {
 }
 
 
-contract PeralToken is owned, TokenERC20 {
+/**
+ * Math operations with safety checks
+ */
+library SafeMath {
+  function mul(uint a, uint b) internal pure returns (uint) {
+    uint c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(uint a, uint b) internal pure returns (uint) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint a, uint b) internal pure returns (uint) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint a, uint b) internal pure returns (uint) {
+    uint c = a + b;
+    assert(c >= a);
+    return c;
+  }
+
+  function max64(uint64 a, uint64 b) internal pure returns (uint64) {
+    return a >= b ? a : b;
+  }
+
+  function min64(uint64 a, uint64 b) internal pure returns (uint64) {
+    return a < b ? a : b;
+  }
+
+  function max256(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a >= b ? a : b;
+  }
+
+  function min256(uint256 a, uint256 b) internal pure returns (uint256) {
+    return a < b ? a : b;
+  }
+}
+
+
+contract PeralToken is Owned, TokenERC20 {
+    using SafeMath for uint256;
 
     /* This generates a public event on the blockchain that will notify clients */
     event FrozenFunds(address target, bool frozen);
@@ -205,12 +253,12 @@ contract PeralToken is owned, TokenERC20 {
     function _transfer(address _from, address _to, uint _value) internal {
         require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
         require (_balanceOf[_from] >= _value);               // Check if the sender has enough
-        require (_balanceOf[_to] + _value > _balanceOf[_to]); // Check for overflows
+        require (_balanceOf[_to].add(_value) > _balanceOf[_to]); // Check for overflows
         require(_closeSale);
         require(!frozenAccount[_from]);                     // Check if sender is frozen
         require(!frozenAccount[_to]);                       // Check if recipient is frozen
-        _balanceOf[_from] -= _value;                         // Subtract from the sender
-        _balanceOf[_to] += _value;                           // Add the same to the recipient
+        _balanceOf[_from] = _balanceOf[_from].sub(_value);                         // Subtract from the sender
+        _balanceOf[_to] = _balanceOf[_to].add(_value);                           // Add the same to the recipient
         Transfer(_from, _to, _value);
     }
 
@@ -219,11 +267,25 @@ contract PeralToken is owned, TokenERC20 {
     /// @param mintedAmount the amount of tokens it will receive
     function mintToken(address target, uint256 mintedAmount) public {
         require(allowMint[msg.sender]);
-        _balanceOf[target] += mintedAmount;
-        _totalSupply += mintedAmount;
+        _balanceOf[target] = _balanceOf[target].add(mintedAmount);
+        _totalSupply = _totalSupply.add(mintedAmount);
         Transfer(0, this, mintedAmount);
         Transfer(this, target, mintedAmount);
     }
+
+    /// @notice Create `mintedAmount` tokens and send it to `target`
+    /// @param target Address to receive the tokens
+    /// @param mintedAmount the amount of tokens it will receive
+    function mintTokenWithUnit(address target, uint256 mintedAmount) public {
+        require(allowMint[msg.sender]);
+        uint256 amount = mintedAmount.mul(TOKEN_UNIT);
+        _balanceOf[target] = _balanceOf[target].add(amount);
+        _totalSupply = _totalSupply.add(amount);
+        Transfer(0, this, mintedAmount);
+        Transfer(this, target, mintedAmount);
+    }
+
+
 
     /// @notice `freeze? Prevent | Allow` `target` from sending & receiving tokens
     /// @param target Address to be frozen
